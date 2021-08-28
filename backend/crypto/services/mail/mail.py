0acfefb85653aa017f19a22254c4473554c8429f
Ahmed_serface <yar2nman@gmail.com>
2021-08-28 21:34:55 +0300

    gridmail of twillow added

--- backend/crypto/services/mail/mail.py
+++ backend/crypto/services/mail/mail.py
@@ -31,3 +31,24 @@ class MailController(BaseMailController):
     def sendMessage(self, msg: Message):
         msg.sender = msg.sender or self.username
         self.mail.send(msg)
+
+def msendemail():
+    # using SendGrid's Python Library
+    # https://github.com/sendgrid/sendgrid-python
+    import os
+    from sendgrid import SendGridAPIClient
+    from sendgrid.helpers.mail import Mail
+
+    message = Mail(
+        from_email='yar2nman@gmail.com',
+        to_emails='to@example.com',
+        subject='Sending with Twilio SendGrid is Fun',
+        html_content='<strong>and easy to do anywhere, even with Python</strong>')
+    try:
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print(e.message)
